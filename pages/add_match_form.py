f109be222ac6d4067e167b590032c47ac4a220c2
KarolinaSosinska <128319284+KarolinaSosinska@users.noreply.github.com>
2023-04-16 19:50:14 +0200

    Update add_match_form.py

--- pages/add_match_form.py
+++ pages/add_match_form.py
@@ -1,17 +1,17 @@
 from pages.base_page import BasePage
 
 
-class DashBoard(BasePage):
-    header_scouts_panel_xpath = "/html/body/div/div/header/div/h6"
-    main_page_button_xpath = "/html/body/div/div[1]/div/div/div/ul[1]/div[1]"
-    players_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[1]/div[2]"
+class AddMatchForm(BasePage):
+    header_scouts_panel_xpath = "/header/div/h6"
+    main_page_button_xpath = "//ul[1]/div[1]"
+    players_button_xpath = "//ul[1]/div[2]"
     selected_player_xpath = "//ul[2]/div[1]"
     matches_xpath = "//ul[2]/div[2]"
     reports_xpath = "//ul[2]/div[3]"
-    language_selection_xpath = "html/body/div/div[1]/div/div/div/ul[3]/div[1]"
-    sign_out_button_xpath = "html/body/div/div[1]/div/div/div/ul[3]/div[2]"
+    language_selection_xpath = "//ul[3]/div[1]"
+    sign_out_button_xpath = "//ul[3]/div[2]"
     submit_button_xpath = "//*[@type='submit']"
-    clear_button_xpath = "/html/body/div/div[1]/main/div[2]/form/div[3]/button[2]"
+    clear_button_xpath = "//button[2]"
     match_at_home_xpath = "//*[@class ='MuiFormControlLabel-root'][1]"
     match_out_home_xpath = "//*[@class ='MuiFormControlLabel-root'][2]"
     my_team_space_xpath = "//*[@name ='myTeam']"
@@ -20,9 +20,9 @@ class DashBoard(BasePage):
     enemy_score_space_xpath = "//*[@name ='enemyTeamScore']"
     tshirt_color_space_xpath = "//*[@name ='tshirt']"
     league_space_xpath = "//*[@name ='league']"
-    time_played_space_xpath = "//*[@type='number' and @name ='timePlayed']"
+    time_played_space_xpath = "//*[@name ='timePlayed']"
     number_space_xpath = "//*[@type='number' and @name= 'number']"
-    web_match_space_xpath = "//*[@id='__next']/div[1]/main/div[2]/form/div[2]/div/div[11]/div/div/input"
-    general_space_xpath = "//*[@class= 'MuiInputBase-input MuiInput-input' and @name='general']"
-    rating_space_xpath = "//*[@min= '0' and @max='5']"
+    web_match_space_xpath = "//div[11]/div/div/input"
+    general_space_xpath = "//*[@name='general']"
+    rating_space_xpath = "//*[@max='5']"
     pass
