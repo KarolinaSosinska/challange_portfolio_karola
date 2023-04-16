56d74b80e5b8a3a9f1eb8c735cbd518881bf691f
KarolinaSosinska <128319284+KarolinaSosinska@users.noreply.github.com>
2023-04-16 19:54:21 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,8 @@
 from pages.base_page import BasePage
 class LoginPage(BasePage):
-    login_field_xpath ="/html/body/div/form/div/div[1]/div[1]/div/input"
-    password_field_xpath ="/html/body/div/form/div/div[1]/div[2]/div/input"
-    sign_in_button_xpath ="/html/body/div/form/div/div[2]/button/span[1]"
+    login_field_xpath ="//div[1]/div/input"
+    password_field_xpath ="//form/div/div[1]/div[2]/div/input"
+    sign_in_button_xpath ="//span[1]"
 
 #
 #     def type_in_email(self, email):
